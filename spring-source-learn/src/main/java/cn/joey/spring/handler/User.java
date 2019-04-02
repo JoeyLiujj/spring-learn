3db16d7458fe610674259f0702984d82e11ca03b
刘吉吉 <904173600@qq.com>
2019-04-02 14:48:47 +0800

    删除groovy内容和增加自定义标签扩展

---
+++ spring-source-learn/src/main/java/cn/joey/spring/handler/User.java
@@ -0,0 +1,35 @@
+package cn.joey.spring.handler;
+
+/**
+ * @auther liujiji
+ * @date 2019/4/1 9:44
+ */
+public class User {
+    private String id;
+    private String userName;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
