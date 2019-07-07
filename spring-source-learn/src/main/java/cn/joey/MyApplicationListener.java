2788c78879e840b75d177aa14029deecb44979f7
JoeyLiu <904173600@qq.com>
2019-07-07 18:23:23 +0800

    springboot事件监听机制修改

---
+++ spring-source-learn/src/main/java/cn/joey/MyApplicationListener.java
@@ -0,0 +1,10 @@
+package cn.joey;
+
+import org.springframework.context.ApplicationListener;
+
+public class MyApplicationListener implements ApplicationListener<MyApplicationEvent>{
+    @Override
+    public void onApplicationEvent(MyApplicationEvent event) {
+        System.out.println(event.getClass().getSimpleName());
+    }
+}
